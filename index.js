6da53ba2d2a13dc6a266fc2f84ce4c42489923d0
UjwlJain <ujjawalpugalia09@gmail.com>
2023-05-27 19:46:57 +0530

    digital date & time strap
    created using html, css & javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+const monthEl = document.querySelector("#date h1");
+const fullDateEl = document.querySelector("#date h3");
+const monthInx = new Date().getMonth();
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+  monthEl.innerText = months[monthInx];
+  fullDateEl.innerText = new Date().toDateString();
+
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+
+function updatetime() {
+    const now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+
+hourEl.innerText = h;
+minuteEl.innerText = m;
+
+setTimeout(() => {
+    updatetime();
+  }, 60000); // update every minute
+}
+
+updatetime();
